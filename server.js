9e98dfec3d4777e22224325668354b6fa668e404
reenye-lima <professor.reenye@gmail.com>
2026-02-13 14:06:38 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+/* Importar e Implementar Rotas */
+// const modelRotas = require("./src/model.routes");
+// app.use(modelRotas);
+/* Fim */
+
+const porta = process.env.PORT_APP || 3000;
+
+app.listen(porta, () => {
+    console.log(`Online na porta ${porta}`);
+});
